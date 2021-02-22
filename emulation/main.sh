5c10f2173a27f10b98e72218d47f2cfebd2888ab
Faridoon Noori <faridoonnoori@gmail.com>
2021-02-22 11:12:59 -0600

    Add code that emulates legacy system

---
+++ emulation/main.sh
@@ -0,0 +1,15 @@
+# Click run above to run the legacy SRPN code
+
+chmod +x ./srpn/srpn
+echo "You can now start interacting with the SRPN calculator"
+./srpn/srpn
+
+# Once you have clicked run try typing in the example input 
+# and observe the output
+# 
+# For example, try
+#
+# 10 
+# 2
+# +
+# =
